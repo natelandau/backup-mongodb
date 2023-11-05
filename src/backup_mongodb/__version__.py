24b023bb1624f532122928150d941eca936c2995
Nathaniel Landau <nate@natelandau.com>
2023-11-05 11:02:56 -0500

    feat: initial commit

---
+++ src/backup_mongodb/__version__.py
@@ -0,0 +1,2 @@
+"""backup-mongodb version."""
+__version__ = "0.0.0"
---
+++ src/backup_mongodb/utils/logging.py
@@ -0,0 +1,55 @@
+"""Logging utilities."""
+import logging
+import sys
+
+from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    """Intercepts standard logging and redirects to Loguru.
+
+    This class is a logging handler that intercepts standard logging messages and redirects them to Loguru, a third-party logging library. When a logging message is emitted, this handler determines the corresponding Loguru level for the message and logs it using the Loguru logger.
+
+    Methods:
+        emit: Intercepts standard logging and redirects to Loguru.
+
+    Examples:
+    To use the InterceptHandler with the Python logging module:
+    ```
+    import logging
+    from logging import StreamHandler
+
+    from loguru import logger
+
+    # Create a new InterceptHandler and add it to the Python logging module.
+    intercept_handler = InterceptHandler()
+    logging.basicConfig(handlers=[StreamHandler(), intercept_handler], level=logging.INFO)
+
+    # Log a message using the Python logging module.
+    logging.info("This message will be intercepted by the InterceptHandler and logged using Loguru.")
+    ```
+    """
+
+    @staticmethod
+    def emit(record):  # type: ignore [no-untyped-def]
+        """Intercepts standard logging and redirects to Loguru.
+
+        This method is called by the Python logging module when a logging message is emitted. It intercepts the message and redirects it to Loguru, a third-party logging library. The method determines the corresponding Loguru level for the message and logs it using the Loguru logger.
+
+        Args:
+            record: A logging.LogRecord object representing the logging message.
+
+        """
+        # Get corresponding Loguru level if it exists.
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
