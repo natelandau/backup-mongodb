b7efc7791bf6aea68054b6e0c996e0dae009ece4
Nathaniel Landau <nate@natelandau.com>
2023-11-05 11:10:29 -0500

    build: remove unused actions and pass tests

--- src/backup_mongodb/__version__.py
+++ src/backup_mongodb/__version__.py
@@ -1,2 +1,2 @@
 """backup-mongodb version."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
---
+++ tests/test_backup.py
@@ -0,0 +1,6 @@
+# type: ignore
+"""Test backup_mongodb.utils.backup.Backup class."""
+
+
+def test_init():
+    pass
